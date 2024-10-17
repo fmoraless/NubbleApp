9f66d2b242752c2db8f9dd22f8b97af56cea65d2
fmoraless <franticzero@gmail.com>
2024-10-17 00:01:08 -0300

    Refactor navigation types and screens imports

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import {AppScreenProps} from 'src/routes/navigationType';
-
 import {Button, Screen, Text} from '@components';
+import {AppScreenProps} from '@routes';
 
 export const SettingsScreen = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
