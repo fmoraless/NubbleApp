a4451b18c9e10858da04d76ec770fade37167ac1
fmoraless <franticzero@gmail.com>
2024-10-16 23:27:05 -0300

    Refactor using generics Type on screens

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {AppStackParamList} from 'src/routes/AppStack';
+import {AppScreenProps} from 'src/routes/navigationType';
 
 import {Button, Screen, Text} from '@components';
 
-type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const SettingsScreen = ({navigation}: ScreenProps) => {
+export const SettingsScreen = (
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  props: AppScreenProps<'SettingsScreen'>,
+) => {
   return (
     <Screen canGoBack>
       <Text preset="headingLarge">Settings Screen</Text>
