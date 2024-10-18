6eab5015aac948f0c15a7bae5218e0aced573339
fmoraless <franticzero@gmail.com>
2024-10-18 19:45:07 -0300

    feat: add post domain layer

---
+++ src/domain/Post/postService.ts
@@ -0,0 +1,10 @@
+import {postApi} from './postApi';
+
+async function getList(): Promise<Post[]> {
+  const postList = await postApi.getList();
+  return postList;
+}
+
+export const postService = {
+  getList,
+};
