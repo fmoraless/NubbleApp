77b0eb326d2d123e2db81110f7f002917931a7cf
fmoraless <franticzero@gmail.com>
2024-10-19 00:49:25 -0300

    feat: post list with FlatList

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,25 +1,45 @@
 import React, {useEffect, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
 import {postService} from '@domain';
 
-import {Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
 import {Post} from '../../../domain/Post/types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const HomeScreen = ({navigation}: AppTabScreenProps<'HomeScreen'>) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [postList, setPostList] = useState<Post[]>([]);
   useEffect(() => {
-    postService.getList().then(list => console.log(list));
+    postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box marginBottom="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32, borderRadius: 16}}
+          />
+          <Text>{item.author.name}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          style={{width: Dimensions.get('screen').width, height: 300}}
+          resizeMode="cover"
+        />
+      </Box>
+    );
+  }
   return (
     <Screen>
-      {postList.map(post => (
-        <Text key={post.id}>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 };
