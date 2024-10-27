d7749affc7845cd99444a003da06114bfe9c96bd
fmoraless <franticzero@gmail.com>
2024-10-27 12:59:59 -0300

    refactor: post comment remove UI integration

--- src/domain/PostComment/useCases/usePostCommentRemove.ts
+++ src/domain/PostComment/useCases/usePostCommentRemove.ts
@@ -1,3 +1,10 @@
-export function usePostCommentRemove() {
-  //TODO: make funtion to remove post comment
+import {MutationOptions, useMutation} from '@infra';
+
+import {postCommentService} from '../postCommentService';
+
+export function usePostCommentRemove(option?: MutationOptions<string>) {
+  return useMutation<{postCommentId: number}, string>(
+    ({postCommentId}) => postCommentService.remove(postCommentId),
+    option,
+  );
 }
--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import {Alert, Pressable} from 'react-native';
 
-import {PostComment} from '@domain';
+import {PostComment, usePostCommentRemove} from '@domain';
 
 import {Box, ProfileAvatar, Text} from '@components';
 
 interface Props {
   postComment: PostComment;
 }
+
 export function PostCommentItem({postComment}: Props) {
+  const {mutate} = usePostCommentRemove();
+
+  function confirmRemove() {
+    Alert.alert('Desea eliminar el comentario?', 'Presione Confirmar', [
+      {text: 'Cancelar', style: 'cancel'},
+      {
+        text: 'Confirmar',
+        onPress: () => mutate({postCommentId: postComment.id}),
+      },
+    ]);
+  }
+
   return (
-    <Box flexDirection="row" alignItems="center" mb="s16">
-      <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box ml="s12" flex={1}>
-        <Text preset="paragraphSmall" bold>
-          {postComment.author.userName}
-        </Text>
-        <Text preset="paragraphSmall" color="gray1">
-          {postComment.message} - {postComment.createdAtRelative}
-        </Text>
+    <Pressable onLongPress={confirmRemove}>
+      <Box flexDirection="row" alignItems="center" mb="s16">
+        <ProfileAvatar imageURL={postComment.author.profileURL} />
+        <Box ml="s12" flex={1}>
+          <Text preset="paragraphSmall" bold>
+            {postComment.author.userName}
+          </Text>
+          <Text preset="paragraphSmall" color="gray1">
+            {postComment.message} - {postComment.createdAtRelative}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Pressable>
   );
 }
