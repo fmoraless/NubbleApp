a2bf8ddce2a688a704f096d2c7c34a399c6b1f60
fmoraless <franticzero@gmail.com>
2024-10-27 02:09:05 -0300

    feat: servicio api remover comment

--- src/domain/PostComment/postCommentApi.ts
+++ src/domain/PostComment/postCommentApi.ts
@@ -4,12 +4,14 @@ import {PageParams} from '../../api/apiTypes';
 
 import {PostCommentAPI} from './postCommentTypes';
 
+const PATH = 'user/post_comment';
+
 async function getList(
   post_id: number,
   pageParams?: PageParams,
 ): Promise<PageAPI<PostCommentAPI>> {
   //await new Promise(resolve => setTimeout(resolve, 2000));
-  const response = await api.get<PageAPI<PostCommentAPI>>('user/post_comment', {
+  const response = await api.get<PageAPI<PostCommentAPI>>(PATH, {
     params: {
       post_id,
       ...pageParams,
@@ -22,14 +24,23 @@ async function create(
   post_id: number,
   message: string,
 ): Promise<PostCommentAPI> {
-  const response = await api.post<PostCommentAPI>('user/post_comment', {
+  const response = await api.post<PostCommentAPI>(PATH, {
     post_id,
     message,
   });
   return response.data;
 }
 
+async function remove(postCommentId: number): Promise<{message: string}> {
+  const response = await api.delete<{message: string}>(
+    `${PATH}/${postCommentId}`,
+  );
+
+  return response.data;
+}
+
 export const postCommentApi = {
   getList,
   create,
+  remove,
 };
