3e3173ecc02b509605644de24056143a63d9279a
fmoraless <franticzero@gmail.com>
2024-10-30 00:04:36 -0300

    refactor: global state zustand

--- src/services/toast/useToast.tsx
+++ src/services/toast/useToast.tsx
@@ -1,6 +1,21 @@
 import {ToastService} from './toastTypes';
-import {useToastContext} from './useToastContext';
+//import {useToastContext} from './useToastContext';
+import {useToastServiceZustand, useToastZustand} from './useToastZustand';
 
-export function useToast(): ToastService {
-  return useToastContext();
+export function useToast(): ToastService['toast'] {
+  // TODO: implementacion con context
+  // const {toast} = useToastContext();
+  // return toast;
+
+  return useToastZustand();
+}
+
+export function useToastService(): Pick<
+  ToastService,
+  'showToast' | 'hideToast'
+> {
+  // TODO: implementacion con context
+  // const {showToast, hideToast} = useToastContext();
+  // return {showToast, hideToast};
+  return useToastServiceZustand();
 }
