b92fa0701f687cfad66f9e4aa021b966c830f41d
fmoraless <franticzero@gmail.com>
2024-11-15 13:15:50 -0300

    feat: enhance authentication flow with loading state and storage management

--- src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
+++ src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createContext, useState} from 'react';
 
-import {AuthCredentials} from '@domain';
+import {AuthCredentials, authService} from '@domain';
 
+import {authCredentialsStorage} from '../authCredentialsStorage';
 import {AuthCredentialsService} from '../authCredentialsTypes';
 
 export const AuthCredentialsContext = createContext<AuthCredentialsService>({
@@ -17,17 +18,37 @@ export function AuthCredentialsProvider({
 }: React.PropsWithChildren<{}>) {
   const [authCredentials, setAuthCredentials] =
     useState<AuthCredentials | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   const [isLoading, setIsloading] = useState(true);
 
-  async function saveCredentials(ac: AuthCredentials): Promise<void> {
-    // TODO: persist
+  useEffect(() => {
+    startAuthCredentials();
+  }, []);
 
+  async function startAuthCredentials() {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 2000, ''));
+      const ac = await authCredentialsStorage.get();
+      if (ac) {
+        authService.updateToken(ac.token);
+        setAuthCredentials(ac);
+      }
+    } catch (error) {
+      // TODO: Handle error
+    } finally {
+      setIsloading(false);
+    }
+  }
+
+  async function saveCredentials(ac: AuthCredentials): Promise<void> {
+    authService.updateToken(ac.token);
+    authCredentialsStorage.set(ac);
     setAuthCredentials(ac);
   }
 
   async function removeCredentials(): Promise<void> {
-    // TODO: persist
+    authService.removeToken();
+    authCredentialsStorage.remove();
     setAuthCredentials(null);
   }
 
