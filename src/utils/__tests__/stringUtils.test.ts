963a43f1f04af238a49e6685beaa3cb64cda69a9
fmoraless <franticzero@gmail.com>
2024-11-30 13:17:31 -0300

    chore: agrouping utility tests

--- src/utils/__tests__/stringUtils.test.ts
+++ src/utils/__tests__/stringUtils.test.ts
@@ -1,12 +1,17 @@
 import {stringUtils} from '@utils';
 
-test('capitalizeFirstLetter', () => {
-  // escribir test
-  stringUtils.capitalizeFirstLetter('Ana maria'); // Ana Maria
-  stringUtils.capitalizeFirstLetter('ANA MARIA'); // Ana Maria
-  stringUtils.capitalizeFirstLetter('maria'); // Maria
-
-  const nombre = stringUtils.capitalizeFirstLetter('Ana maria');
-
-  expect(nombre).toBe('Ana Maria');
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ANA MARIA')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('maria')).toBe('Maria');
+      expect(stringUtils.capitalizeFirstLetter('ANA')).toBe('Ana');
+    });
+    it('should remove white leading and trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter(' Ana maria ')).toBe(
+        'Ana Maria',
+      );
+    });
+  });
 });
