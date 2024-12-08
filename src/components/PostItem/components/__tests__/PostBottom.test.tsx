f488b7e6ca18d05a7a89adc3db4723e2da1161fe
fmoraless <franticzero@gmail.com>
2024-12-07 21:33:47 -0300

    test: enhance PostBottom tests to verify comment link navigation and visibility based on comment count

--- src/components/PostItem/components/__tests__/PostBottom.test.tsx
+++ src/components/PostItem/components/__tests__/PostBottom.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {Post} from '@domain';
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
 import {PostBottom} from '../PostBottom';
 
@@ -20,11 +20,39 @@ const mockedPost: Post = {
   },
 };
 
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
+
 describe('<PostBottom />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('it does not show the comment link if it has no comments', () => {
     render(<PostBottom {...mockedPost} commentCount={0} />);
 
     const commentLinkElement = screen.queryByText(/comentario/);
     expect(commentLinkElement).toBeFalsy();
+  });
+  it('navigates to Post comment screen when pressing the comment link', () => {
+    render(<PostBottom {...mockedPost} commentCount={4} />);
+
+    const commentLinkElement = screen.getByText(/comentario/);
+
+    fireEvent.press(commentLinkElement);
+
+    expect(mockedNavigate).toHaveBeenCalledWith('PostCommentScreen', {
+      postId: mockedPost.id,
+      postAuthorId: mockedPost.author.id,
+    });
   });
 });
