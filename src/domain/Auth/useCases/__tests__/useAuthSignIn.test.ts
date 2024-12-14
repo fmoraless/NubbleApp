616b59d3d9fe18aabc3fd73c2bb75aace2925a84
fmoraless <franticzero@gmail.com>
2024-12-14 16:37:44 -0300

    test: add mock for saveCredentials in useAuthSignIn tests

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -4,18 +4,35 @@ import {AllTheProviders, waitFor} from 'test-utils';
 import {authService} from '../../authService';
 import {useAuthSignIn} from '../useAuthSignIn';
 
+const mockedSaveCredentials = jest.fn();
+
 import {mockedAuthCredentials} from './mockedData/mocks';
+
+jest.mock('@services', () => {
+  const originalModule = jest.requireActual('@services');
+  return {
+    ...originalModule,
+    useAuthCredentials: () => ({
+      saveCredentials: mockedSaveCredentials,
+    }),
+  };
+});
 
 describe('useAuthSignIn', () => {
   it('save credentials if the sign-in successfully', async () => {
     jest
       .spyOn(authService, 'signIn')
       .mockResolvedValueOnce(mockedAuthCredentials);
+
     const {result} = renderHook(() => useAuthSignIn(), {
       wrapper: AllTheProviders,
     });
+
     result.current.signIn({email: 'test@test', password: '1234'});
+
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(mockedSaveCredentials).toHaveBeenCalledWith(mockedAuthCredentials);
   });
   it('calls the onError with a message if sign-in fails', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
