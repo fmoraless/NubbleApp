61bbbec1865af73a18e470d5f0b295e63ba94ad0
fmoraless <franticzero@gmail.com>
2024-12-15 00:24:47 -0300

    test: custom render hook

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {render, RenderOptions} from '@testing-library/react-native';
+import {
+  render,
+  renderHook,
+  RenderHookOptions,
+  RenderOptions,
+} from '@testing-library/react-native';
 
 import {theme} from '@theme';
-const queryClient = new QueryClient({
-  logger: {
-    log: console.log,
-    warn: console.warn,
-    // ✅ no more errors on the console for tests
-    //@ts-ignore
-    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
-  },
-  defaultOptions: {
-    queries: {
-      retry: false,
-      cacheTime: Infinity,
-    },
-    mutations: {
-      retry: false,
-      cacheTime: Infinity,
-    },
-  },
-});
 
-export const AllTheProviders = ({children}: {children: React.ReactNode}) => {
-  return (
+export const wrapperAllProviders = () => {
+  const queryClient = new QueryClient({
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      // ✅ no more errors on the console for tests
+      //@ts-ignore
+      error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+    },
+    defaultOptions: {
+      queries: {
+        retry: false,
+        cacheTime: Infinity,
+      },
+      mutations: {
+        retry: false,
+        cacheTime: Infinity,
+      },
+    },
+  });
+
+  return ({children}: {children: React.ReactNode}) => (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <NavigationContainer>{children}</NavigationContainer>
@@ -37,11 +43,22 @@ export const AllTheProviders = ({children}: {children: React.ReactNode}) => {
 };
 
 function customRender<T = unknown>(
-  component: React.ReactElement<T>,
+  component: ReactElement<T>,
   options?: Omit<RenderOptions, 'wrapper'>,
 ) {
-  return render(component, {wrapper: AllTheProviders, ...options});
+  return render(component, {wrapper: wrapperAllProviders(), ...options});
+}
+
+function customRenderHook<Result, Props>(
+  renderCallback: (props: Props) => Result,
+  options?: Omit<RenderHookOptions<Props>, 'wrapper'>,
+) {
+  return renderHook(renderCallback, {
+    wrapper: wrapperAllProviders(),
+    ...options,
+  });
 }
 
 export * from '@testing-library/react-native';
 export {customRender as render};
+export {customRenderHook as renderHook};
