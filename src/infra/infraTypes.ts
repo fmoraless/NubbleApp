83e78e37bfef5892bfb38cd87387c7f67216cc07
fmoraless <franticzero@gmail.com>
2025-01-04 20:45:11 -0300

    feat: implement user search functionality with debounced input and pagination support

--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -2,6 +2,7 @@ export enum QueryKeys {
   UserGetById = 'UserGetById',
   PostCommentList = 'PostCommentList',
   PostList = 'PostList',
+  UserList = 'UserList',
   IsUserNameAvailable = 'IsUserNameAvailable',
   IsEmailAvailable = 'IsEmailAvailable',
 }
--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,21 +1,33 @@
 import React, {useState} from 'react';
 
+import {useUserSearch} from '@domain';
+
 import {Icon, Screen, Text, TextInput} from '@components';
+import {useDebounce} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search);
+
+  const {list} = useUserSearch(debouncedSearch);
+  console.log('list', list);
+
   return (
     <Screen
       canGoBack
       HeaderComponent={
         <TextInput
+          placeholder="Buscar"
           value={search}
           onChangeText={setSearch}
           LeftComponent={<Icon name="search" color="gray3" />}
         />
       }>
       <Text>SearchScreen</Text>
+      {list.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Screen>
   );
 }
