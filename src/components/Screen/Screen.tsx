fc1f6674833e1320bc1fc812db7088c55acabf2e
fmoraless <franticzero@gmail.com>
2025-01-19 21:12:23 -0300

    feat: add noPaddingHorizontal prop to Screen component for flexible padding

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -13,6 +13,7 @@ export interface ScreenProps extends BoxProps {
   canGoBack?: boolean;
   scrollable?: boolean;
   title?: string;
+  noPaddingHorizontal?: boolean;
 }
 
 export function Screen({
@@ -20,6 +21,7 @@ export function Screen({
   canGoBack = false,
   scrollable = false,
   title,
+  noPaddingHorizontal = false,
   HeaderComponent,
   style,
   ...boxProps
@@ -35,7 +37,7 @@ export function Screen({
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Container backgroundColor={colors.background}>
         <Box
-          paddingHorizontal="s24"
+          paddingHorizontal={noPaddingHorizontal ? undefined : 's24'}
           style={[
             {
               paddingTop: top,
