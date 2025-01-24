7723dbc0435479856c7fcb2a4c564e423ac06f7d
fmoraless <franticzero@gmail.com>
2025-01-24 01:03:56 -0300

    feat: add permission service with types for handling permissions

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://192.168.1.81:3333/';
+export const BASE_URL = 'http://192.168.1.82:3333/';
 export const api = axios.create({
   baseURL: BASE_URL,
 });
