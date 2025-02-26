c126b0d45ca22019f6749a8d72f4a3601b4ef287
fmoraless <franticzero@gmail.com>
2025-02-26 19:27:41 -0300

    feat: implement settings service for theme preference management

---
+++ src/services/settings/settingsService.ts
@@ -0,0 +1,23 @@
+import {Appearance, ColorSchemeName} from 'react-native';
+
+import {ThemePreference, AppTheme} from './settingsType';
+function onThemePreference(themePreference: ThemePreference): AppTheme {
+  if (themePreference === 'system') {
+    const colorScheme = Appearance.getColorScheme();
+
+    return colorScheme ? colorScheme : 'light';
+  }
+  return themePreference;
+}
+
+function onSystemChange(
+  color: ColorSchemeName,
+  themePreference: ThemePreference,
+): AppTheme | null {
+  if (themePreference === 'system') {
+    return color ? color : 'light';
+  }
+  return null;
+}
+
+export const settingsService = {onThemePreference, onSystemChange};
