f3c8c81accf9b5afcc53c1c0d8e87459959e97fc
fmoraless <franticzero@gmail.com>
2025-03-09 20:19:16 -0300

    feat: add onboarding images for light and dark themes

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,9 @@
 export const images = {
   imagePlaceholder: require('./images/image_placeholder.png'),
+  onbardingLight1: require('./images/onboarding/onboarding-light-1.png'),
+  onbardingLight2: require('./images/onboarding/onboarding-light-2.png'),
+  onbardingLight3: require('./images/onboarding/onboarding-light-3.png'),
+  onbardingDark1: require('./images/onboarding/onboarding-dark-1.png'),
+  onbardingDark2: require('./images/onboarding/onboarding-dark-2.png'),
+  onbardingDark3: require('./images/onboarding/onboarding-dark-3.png'),
 };
